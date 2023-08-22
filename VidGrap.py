1aa20bd771d7236141555f22c127bd3252b11cab
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-08-22 10:09:39 +0200

    Update VidGrap.py

--- VidGrap.py
+++ VidGrap.py
@@ -15,9 +15,6 @@ from AD_func import *
 
 print('Press ESC to close the window')
 
-arduinoSer = serial.Serial('COM3',baudrate=9600,timeout=0.5)
-flow_on = 0;
-
 # conecting to the first available camera
 camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 
@@ -31,7 +28,7 @@ converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
 #cap = cv2.VideoCapture(0) #VideoCapture object which stores the frames, the argument is just the device index (may be 0, or -1)
 size = (4504, 4504) # Camera resoloution: 4504x4504px, FPS: 18
-fourcc = cv2.VideoWriter_fourcc('M','J','P','G') #Defines output format, mp4
+fourcc = cv2.VideoWriter_fourcc(*'MJPG') #Defines output format, mp4
 out = cv2.VideoWriter('Algae_Vid.mp4', fourcc, 18.0, size)
 
 while camera.IsGrabbing():
@@ -43,31 +40,30 @@ while camera.IsGrabbing():
         img = image.GetArray() # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
         cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
         cv2.imshow('Algae experiment', img)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        
         k = cv2.waitKey(1)
         
-        if k == ord('r'):
-            out.write(img)
-
-
-
-
-
-
+        if k == ord('r'): # press down r to record
+            record = True
+            if record:    
+                out.write(img, gray)
+                time.sleep(10)
+                record = not record
+        elif k == ord('f'): # press f
+            funcGen()
+        elif k == ord('F'): # press F
+            funcGen(freq=3.82)
+        elif k == ord('g'): # press g
+            funcStop()
+# Close experiment
         if k == 27: # press ESC
             cv2.destroyAllWindows()
             break
-            
-        
-        #if k == ord('f'): # press f
-        #    funcGen()
-        #if k == ord('F'): # press F
-        #    funcGen(freq=3.82)
-        #elif k == ord('g'): # press g
-        #    funcStop()
-   
+
     grabResult.Release()
-    out.Release()
+
     
 # Releasing the resource    
 camera.StopGrabbing()
-
+out.Release()
