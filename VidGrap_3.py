70438195e56a562f65564c4ddf82048cba8ab13d
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-08-24 13:03:32 +0200

    Made a new scirpt using np

--- VidGrap_3.py
+++ VidGrap_3.py
@@ -1,54 +1,29 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Aug 23 10:41:26 2023
+Created on Thu Aug 24 12:58:32 2023
 
-@author: Phili
+@author: joakimpihl
 """
 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 21 10:55:36 2023
+
+@author: joakimpihl
+"""
+
+# Code for generating the first recording op particle focusing to be used for practice of image analysis
 
 from pypylon import pylon
+import serial, time
 import cv2
-import sys
-sys.path.append('C:/Users/s102772/Desktop/Algae_Python')
-from AD_func import *
-
-###############################################################################
-### For controlling labb setup.
-
-from ThorlabsFunctions import *
 import numpy as np
-from pypylon import pylon
-import cv2
 import sys
 sys.path.append('C:/Users/s102772/Desktop/Algae_Python')
 from AD_func import *
 
-###############################################################################
-### Move to position
-
-#HomeX()
-#HomeY()
-#HomeZ()
-
-print("The device is homed in all directions")
-
-### Input the starting position. Make sure the device hs not been moved since
-### last use.
-
-coordinate =  [30, 20, 5]
-
-MoveRelX(coordinate[0])
-MoveRelY(coordinate[1])
-MoveRelZ(coordinate[2])
-
-start_coordinate = [PositionX(), PositionY(), PositionZ()]
-
-print("The device is now in the starting position")
-
-###############################################################################
-### Check the tilt of the device
-
-## Initialize camara
 print('Press ESC to close the window')
 
 # conecting to the first available camera
@@ -78,27 +53,24 @@ while camera.IsGrabbing():
     if grabResult.GrabSucceeded():
         # Access the image data
         image = converter.Convert(grabResult)
-        img = image.GetArray() # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
+        img = np.array(image) # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
+        
+        #Crop image to ROI
+        #crop_img_array = 
+        
         cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
         cv2.imshow('Algae experiment', img)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         k = cv2.waitKey(1)
         
         
-        #Philip smid din thorlabs kode herind
-        if k == ord('t'):
-            #THorlabs kode
-            print()
-
-
-
-        
         if k == ord('r'): # press down r to record
             record = True
             print('Starting recording..')
-        elif k == ord('s'): #press s to stop recording
+        elif k == ord('s'): #Stop recording
             record = False    
             print('Stopping recording..')
+        
         if record:
             out.write(img)
              
@@ -122,42 +94,4 @@ while camera.IsGrabbing():
 # Releasing the resource   
 out.release() 
 camera.StopGrabbing()
-
-
-
-# Moving Thorslab to top 
-
-
-
-while dot == False:
-    picture_hight = 1
-    MoveAbsZ(picture_hight)
-    
-    ## Something camara / find dot
-    if something:
-        dot = True
-## Something camara / focus
-end_coordinate = [PositionX(), PositionY(), PositionZ()]
-
-# Calculating the angle tilt
-tilt = end_coordinate[2] - start_coordinate[2] / end_coordinate[1] - start_coordinate[1]
-angle = np.arctan(tilt)
-
-print("The tilt angle is " +str(angle))
-
-# Move to start
-
-MoveAbsX(start_coordinate[0], start_coordinate[1], start_coordinate[2])
-
-print("The tilt check is done and the device is back in starting position")
-
-###############################################################################
-### Doing analysis of capillary tube
-
-## Initialize camara
-
-# Move in position for analysis
-
-###############################################################################
-
 
