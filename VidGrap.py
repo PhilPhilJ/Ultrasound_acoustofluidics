e39bfd5cde8e4520b4c43a5f42d5fd7d365e6458
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-09-04 16:41:05 +0200

    VidGrap_2
    Contains test img analysis script

--- VidGrap.py
+++ VidGrap.py
@@ -37,8 +37,6 @@ out = cv2.VideoWriter('C:/Users/s102772/Desktop/Algae_Vid_7.mp4', fourcc, FPS, s
 
 #Connect to analog discovery
 Connect()
-t = 1
-
 
 while camera.IsGrabbing():
     grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
@@ -52,76 +50,32 @@ while camera.IsGrabbing():
         cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
         cv2.imshow('Algae experiment', img)
         k = cv2.waitKey(1)
+        
+        
         if k == ord('r'): # press down r to record
             record = True
             print('Starting recording..')
         elif k == ord('s'): #Stop recording
             record = False    
             print('Stopping recording..')
+        
         if record:
             out.write(img)
+             
+            
         if k == ord('f'): # press f
             funcGen()
         if k == ord('p'): # press F
             funcGen(freq=3.82)
         if k == ord('g'): # press g
             funcStop()
-            
-################################### Deletes unwanted data
-        #average the video along y-dricetion so that we get an 1D array/vector
-        frame_1d = np.array(np.mean(img, axis=0))
-        
-        if t == 1:
-            grad = abs(np.diff(frame_1d))
-            
-            grad[grad<=0.5] = 0
-            grad[grad>0.5] = 1
-            
-            plt.plot(np.arange(0,len(frame_1d)-2), grad[0:4502])
-            
-            img_mid = int(np.round(len(grad)/2)) #Midpoint of the image
-            
-            index_left_side = np.array([])
-            index_right_side = np.array([])
-            
-            for (i,j) in enumerate(np.diff(grad[0:img_mid])):
-                if j < 0:
-                    index_left_side = np.append(index_left_side,[i])
-                    
-            for (i,j) in enumerate(np.diff(grad)):
-                if (j > 0) and (i >= img_mid):
-                    index_right_side = np.append(index_right_side,[i])
-            
-            index_left_side = int(np.max(index_left_side))
-            index_right_side = int(np.min(index_right_side))
-            
-        frame = frame[index_left_side: index_right_side]
-        frame_1d = frame_1d[index_left_side: index_right_side]
-            
-        ave_frame_cut = np.round(np.tile(frame_1d, (len(frame_1d),1)))/255 #Cut frame avereaged along y-driection
-            
-        cv2.namedWindow("Frame - Area of interest", cv2.WINDOW_NORMAL)
-        cv2.imshow("Frame - Area of interest", ave_frame_cut)     
-
-###########################################         
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
 # Close experiment
         if k == 27: # press ESC
             cv2.destroyAllWindows()
             disconnect()
             break
 
-    t+=1
+
     grabResult.Release()
 
     
