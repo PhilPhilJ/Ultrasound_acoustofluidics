0bd212b077d3461b34b074b1ed1c1ae3ced11dab
PhilPhilJ <113350944+PhilPhilJ@users.noreply.github.com>
2023-10-04 14:40:39 +0200

    Update Pylon_FPS.py

--- Pylon_FPS.py
+++ Pylon_FPS.py
@@ -17,22 +17,21 @@ camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
 camera.Open()
 
 # to get consistant results it is always good to start from "power-on" state
-camera.UserSetSelector = "Default"
-camera.UserSetLoad.Execute()
+#camera.UserSetSelector = "Default"
+#camera.UserSetLoad.Execute()
 
-camera.ExposureTime = camera.ExposureTime.Min
-print('Exposure time of camera' + camera.ExposureTime)
 
+#%%
 # The parameter MaxNumBuffer can be used to control the count of buffers
 # allocated for grabbing. The default value of this parameter is 10.
-camera.MaxNumBuffer = 15
+camera.MaxNumBuffer = 30
 
 
 ######################## Grabbing strategy ########################
 
 # The GrabStrategy_OneByOne strategy is used. The images are processed
 # in the order of their arrival.
-camera.StartGrabbing(pylon.GrabStrategy_OneByOne)
+#camera.StartGrabbing(pylon.GrabStrategy_OneByOne)
 
 
 
@@ -42,9 +41,7 @@ camera.StartGrabbing(pylon.GrabStrategy_OneByOne)
 # This strategy can be useful when the acquired images are only displayed on the screen.
 # If the processor has been busy for a while and images could not be displayed automatically
 # the latest image is displayed when processing time is available again.
-# =============================================================================
-# camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
-# =============================================================================
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
 
 ######################## Grabbing strategy ########################
 
@@ -62,43 +59,64 @@ FPS = 18 # Frames per second of camera
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') #Defines output format, mp4
 out = cv2.VideoWriter('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4', fourcc, FPS, size) #Change path to saved location
 
-start = time.time()
 delta_t = 10 #How long should the recording be
+runs = 10
+fps = np.zeros(runs) 
 
-record = -10
-start = time.time()
-while camera.IsGrabbing():
-    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
-    if grabResult.GrabSucceeded():
-        # Access the image data
-        image = converter.Convert(grabResult)
-        img = image.GetArray() # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
-        stop = time.time()
-        
-        #cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Converts img to gray-scale
-        cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
-        cv2.imshow('Algae experiment', img)
-        
-        if not 'start' in globals(): #If the variable start doesn't exist define a start time and record
-            start = time.time()
-            start_frame = camera.get(cv2.CAP_PROP_FRAME_COUNT)
-            record = True
-            print('Starting timing')
-        elif stop-start>=delta_t: #Stop recording
-            record = False    
-            stop_frame = camera.get(cv2.CAP_PROP_FRAME_COUNT)
-            print('Stop timing')
-            print('Stop-start= ' + str(stop-start) + ' s')
-            print('# of frames collected: ' + str(stop_frame-start_frame))
-            print('FPS: '+str((stop_frame-start_frame)/(stop-start)))
-            break
-        if record:
-            out.write(img)
-            
-        
-    grabResult.Release()
+for i in range(runs):
+    if i <= runs:
+        if 'start' in globals():
+            del start
+        while camera.IsGrabbing():
+            grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+            if grabResult.GrabSucceeded():
+                # Access the image data
+                image = converter.Convert(grabResult)
+                img = image.GetArray() # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
+                stop = time.time()
+                
+                #cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Converts img to gray-scale
+                cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
+                cv2.imshow('Algae experiment', img)
+                
+                if not 'start' in globals(): #If the variable start doesn't exist define a start time and record
+                    start = time.time()
+                    print('Starting timing')
+                elif stop-start>=delta_t: #Stop recording   
+                    print('Stop timing')
+                    print('Stop-start= ' + str(stop-start) + ' s')
+                    break
+                if 'start' in globals() and stop-start<delta_t:
+                    out.write(img)
+                    
+        out.release()          
+        vid = cv2.VideoCapture('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4') #Loads the video
 
+        n_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+        fps[i] = n_frames/(stop-start)        
+                
+        grabResult.Release()
     
-# Releasing the resource   
-out.release() 
-camera.StopGrabbing()
+    else:
+        grabResult.Release()
+        # Releasing the resource   
+        out.release() 
+        camera.StopGrabbing()
+
+#%%
+vid = cv2.VideoCapture('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4') #Loads the video
+
+n_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+FPS = round(np.mean(fps),2)
+
+print('mean FPS: ' + str(FPS))
+print('sd: ' + str(round(np.std(fps),2)))
+
+
+
+
+
+
+
+
+
