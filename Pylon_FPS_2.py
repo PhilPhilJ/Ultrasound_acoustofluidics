773f25f8b8c2656c0e06412d2e73daa2d1313023
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-10-10 14:22:39 +0200

    Update Pylon_FPS_2.py

--- Pylon_FPS_2.py
+++ Pylon_FPS_2.py
@@ -26,12 +26,14 @@ converter = pylon.ImageFormatConverter()
 converter.OutputPixelFormat = pylon.PixelType_BGR8packed
 converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
-size = (4504, 4504) # Camera resoloution: 4504x4504px, FPS: 18
-FPS = 18 # Frames per second of camera
-fourcc = cv2.VideoWriter_fourcc(*'mp4v') #Defines output format, mp4
-out = cv2.VideoWriter('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4', fourcc, FPS, size) #Defines a path to where the movie is saved, video format, playback framerate, and video size/resolution
-
-video = np.empty(size) #Creates a matrix to store the video file
+# =============================================================================
+# size = (4504, 4504) # Camera resoloution: 4504x4504px, FPS: 18
+# FPS = 18 # Frames per second of camera
+# fourcc = cv2.VideoWriter_fourcc(*'mp4v') #Defines output format, mp4
+# out = cv2.VideoWriter('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4', fourcc, FPS, size) #Defines a path to where the movie is saved, video format, playback framerate, and video size/resolution
+# 
+# =============================================================================
+video = np.empty(4504, 4504, 1) #Creates a matrix to store the video file
 
 delta_t = 10 #How long should the recording be, measured in seconds
 
@@ -41,13 +43,13 @@ while camera.IsGrabbing():
       grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
       if grabResult.GrabSucceeded():
           # Access the image data
-          image = converter.Convert(grabResult)
-          img = image.GetArray() # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
-          video = np.append(video, img, axis=2)
+          #image = converter.Convert(grabResult)
+          #img = image.GetArray() # Array of size (4504, 4504, 3) = (pixel, pixel, rgb)
+          video = np.append(video, image, axis=2)
           stop = time.time() 
                 
-          cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
-          cv2.imshow('Algae experiment', img)
+          #cv2.namedWindow('Algae experiment', cv2.WINDOW_NORMAL)
+          #cv2.imshow('Algae experiment', img)
 
       if not 'start' in globals(): #If the variable start doesn't exist define a start time and start recording
           start = time.time()
@@ -68,9 +70,10 @@ camera.StopGrabbing()
 
 #%%
 # This part loads the video that the script created and computes a framerate (FPS) based on the movie
-vid = cv2.VideoCapture('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4') #Loads the video
+#vid = cv2.VideoCapture('C:/Users/s102772/Desktop/Algae_Vid_Exp.mp4') #Loads the video
 
-n_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+#n_frames = vid.get(cv2.CAP_PROP_FRAME_COUNT)
+n_frames = np.size(video, axis=2)
 FPS = round(n_frames/(stop-start),2)
 
 print('FPS: ' + str(FPS))
