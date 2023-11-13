cf63214977c8fe09032b1af637d146e6a89cd655
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-11-13 11:20:27 +0100

    Update Background invert.py

--- Image analysis/Background invert.py
+++ Image analysis/Background invert.py
@@ -10,8 +10,8 @@ import numpy as np
 import time
 
 # Define the cropping coordinates
-x1, y1 = 1250, 1000
-x2, y2 = 2400, 3000
+x1, y1 = 1104, 0
+x2, y2 = 3386, 4504
 
 # Read the input video
 input_video = cv2.VideoCapture(r"C:\Users\Phili\OneDrive - Danmarks Tekniske Universitet\Bachelorprojekt\Videoer\Algae_Vid_5.mp4")
@@ -27,10 +27,8 @@ output_video = cv2.VideoWriter(r"C:\Users\Phili\OneDrive - Danmarks Tekniske Uni
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 # Process the video frames
-start_time = time.time()
 while True:
     ret, frame = input_video.read()
-
     if not ret:
         break
 
@@ -40,46 +38,28 @@ while True:
     # Write the cropped frame to the output video
     output_video.write(cropped_frame)
 
-    #Display the cropped frame (optional)
-    #cv2.imshow('Cropped Video', cropped_frame)
-
-    # Exit if 'q' key is pressed
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-end_time = time.time()
-print("Compute time is " + str(end_time - start_time))
-
 output_video.release()
 input_video.release()
-cv2.destroyAllWindows()
-
 # Read the input video
 cap = cv2.VideoCapture(r"C:\Users\Phili\OneDrive - Danmarks Tekniske Universitet\Bachelorprojekt\Videoer\Algae_Vid_6crop and back.mp4")
 fgbg = cv2.createBackgroundSubtractorMOG2()
 
 # Set up the output video
 fps = int(cap.get(cv2.CAP_PROP_FPS))
-#cv2.namedWindow('frame',0)
-#cv2.resizeWindow('frame',300,300)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(r"C:\Users\Phili\OneDrive - Danmarks Tekniske Universitet\Bachelorprojekt\Videoer\Algae_Vid_6croppedBW.mp4",fourcc, fps,(x2 - x1, y2 - y1),False)
 
 while True:
     ret, frame = cap.read()
-    if frame is None:
+    if not ret:
         break
     fgmask = abs(255-fgbg.apply(frame))
-    #fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)
 
-    #cv2.imshow('Frame', frame)
-    #cv2.imshow('FG MASK Frame', fgmask)
     out.write(fgmask)
 
     keyboard = cv2.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
         break
 
-
 out.release()
 cap.release()
-cv2.destroyAllWindows()
