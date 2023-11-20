d45a204b139e77ecb2c91882fc941383f8251ad3
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-11-20 14:06:57 +0100

    Made image analysis script iterative

--- Image analysis/OneOverFocusTime.py
+++ Image analysis/OneOverFocusTime.py
@@ -12,43 +12,43 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
 
-df = pd.read_csv('/Users/joakimpihl/Desktop/DTU/7. Semester/Bachelorprojekt/Resonance freq - Exp 2/Resonance freq - Exp 2.csv', delimiter=';', encoding='utf-8')
+df = pd.read_csv('/Users/joakimpihl/Desktop/DTU/7. Semester/Bachelorprojekt/Focus sweep 4  - Plots/Background - R fit/Resonance freq - Exp 4.csv', delimiter=';', encoding='utf-8')
 freq = np.array(df['Frequency (MHz)'].tolist())
 focus_time = np.array(df['Focusing time (99.99%)'].tolist())
+#Unusually quick focussing time at 1.84MHz
+focus_time = np.delete(focus_time, 0)
+freq = np.delete(freq, 0)
 
-#%%
+#Remove negative values from data
+focus_time_new = np.empty(0)
+freq_new = np.empty(0)
+
+for i,j in enumerate(focus_time):
+    if j>0:
+        focus_time_new = np.append(focus_time_new, j)
+        freq_new = np.append(freq_new, freq[i])
+
+#Fitting to lorentzian peak
 def func(x, x0, w, h):
     return (h*w**2)/(w**2+(x-x0)**2)
 
-popt, pcov = curve_fit(func, freq, (1/focus_time), bounds=(0.1, [1.9, 10, 0.5]))
 # =============================================================================
-# popt_high, pcov_high = curve_fit(func, freq[0:9], (1/focus_time[0:9]), bounds=(0.1, [1.9, 10, 0.5]))
-# popt_low, pcov_low = curve_fit(func, freq[10:-1], (1/focus_time[10:-1]))
-# =============================================================================
-
-xdata = np.linspace(1.85, 1.93, 1000)
-
-ydata = func(xdata, *popt) ## Curvefit doesn't work - too few data points??
-# =============================================================================
-# ydata_high = func(xdata, *popt_high) ## Curvefit doesn't work - too few data points??
-# ydata_low = func(xdata, *popt_low)
-# =============================================================================
-# =============================================================================
-# ydata_high = func(xdata, 1.885, 0.03, 0.16)
-# ydata_low = func(xdata, 1.896, 0.03, 0.065)
+# popt, pcov = curve_fit(func, freq_new, 1/focus_time_new, bounds=(0.1, [1.9, 10, 0.5]))
 # 
+# #Generate data from fit
+# xdata = np.linspace(1.85, 1.95, 1000)
+# 
+# ydata = func(xdata, *popt) ## Curvefit doesn't work - too few data points??
+# 
+# #Plotting 1/t vs. freq
+# fig, ax = plt.subplots(figsize=(10,10))
+# ax.plot(freq_new, 1/focus_time_new, 'ro', markersize='10')
+# ax.plot(xdata, ydata, 'r--')
+# 
+# ax.set_xlabel(r'Frequency [MHz]', fontsize='17.5')
+# ax.set_ylabel(r'1/Time [$\mathrm{s}^{-1}$]', fontsize='17.5')
 # =============================================================================
-#%%
-#Plotting 1/t vs. freq
 
-fig, ax = plt.subplots(figsize=(10,10))
-ax.plot(freq, 1/focus_time, 'ro', markersize='10')
-ax.plot(xdata, ydata, 'r--')
-# =============================================================================
-# ax.plot(freq[0:9], 1/focus_time[0:9], 'ro', markersize='10')
-# ax.plot(freq[10:-1], 1/focus_time[10:-1], 'b+', markersize='12')
-# ax.plot(xdata, ydata_high, 'r--')
-# ax.plot(xdata, ydata_low, 'b--')
-# =============================================================================
-ax.set_xlabel(r'Frequency [MHz]', fontsize='17.5')
-ax.set_ylabel(r'1/Time [$\mathrm{s}^{-1}$]', fontsize='17.5')
+#Generate csv file with filtered data
+df_write = pd.DataFrame({'Frequency (MHz)':freq_new, 'Focusing time (99.99%)':focus_time_new})
+df_write.to_csv('/Users/joakimpihl/Desktop/DTU/7. Semester/Bachelorprojekt/Focus sweep 4  - Plots/data for peak fit/Background - R fit.csv', sep=',')
