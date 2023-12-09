46921b4e301cc8c6dff7924381d67bc7d89bf198
JEPihl <141318950+JEPihl@users.noreply.github.com>
2023-12-09 16:51:18 +0100

    Minor changes to image analysis script
    The script now creates a .csv file which contains both the time shifted timestamp as well as the normalized intensities.

--- Image analysis/OneOverFocusTime.py
+++ Image analysis/OneOverFocusTime.py
@@ -12,42 +12,55 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
 
-df = pd.read_csv('/Users/joakimpihl/Desktop/DTU/7. Semester/Bachelorprojekt/Results/Focus sweep 6/Resonance freq.csv', delimiter=',', encoding='utf-8')
+df = pd.read_csv('/Users/joakimpihl/Desktop/DTU/7. Semester/Bachelorprojekt/Results/Focus sweep 6.3/Resonance freq.csv', delimiter=',', encoding='utf-8')
 freq = np.array(df['Frequency (MHz)'].tolist())
-focus_time = np.array(df['Focusing time (99.99%)'].tolist())
+t_star = np.array(df['t_star'].tolist())
+reciprocal_t_star = 1/t_star
+error = np.array(df['St dev on t_star'].tolist())
 #Unusually quick focussing time at 1.84MHz
 #focus_time = np.delete(focus_time, 0)
 #freq = np.delete(freq, 0)
-
+#%%
 #Remove negative values from data
-focus_time_new = np.empty(0)
-freq_new = np.empty(0)
+# focus_time_new = np.empty(0)
+# freq_new = np.empty(0)
 
-for i,j in enumerate(focus_time):
-    if j>0:
-        focus_time_new = np.append(focus_time_new, j)
-        freq_new = np.append(freq_new, freq[i])
-
+# for i,j in enumerate(focus_time):
+#     if j>0:
+#         focus_time_new = np.append(focus_time_new, j)
+#         freq_new = np.append(freq_new, freq[i])
+#%%
 #Fitting to lorentzian peak
-def func(x, x0, w, h):
-    return (h*w**2)/(w**2+(x-x0)**2)
+def func(x, x1, w1, h1): #x2, w2, h2):
+    return (h1*w1**2)/(w1**2+(x-x1)**2)# + (h2*w2**2)/(w2**2+(x-x2)**2)
 
-popt, pcov = curve_fit(func, freq_new, 1/focus_time_new, bounds=([1.88, 0, 0], [1.91, 0.05, 0.5]))
+#popt, pcov = curve_fit(func, freq_new, 1/focus_time_new, bounds=([1.88, 0, 0], [1.91, 0.05, 0.5]))
+popt, pcov = curve_fit(func, freq, reciprocal_t_star, bounds=([1.895, 0, 0], [1.90, 0.05, 2])) #bounds=([1.887, 0, 0, 1.895, 0, 0], [1.89, 0.05, 2, 1.90, 0.05, 2]
+
 
 #Generate data from fit
-xdata = np.linspace(1.85, 1.95, 1000)
+xdata = np.linspace(min(freq)-0.001, max(freq)+0.001, 1000)
 
-ydata = func(xdata, *popt) ## Curvefit doesn't work - too few data points??
+ydata = func(xdata, *popt) 
 
+residuals = reciprocal_t_star - func(freq, *popt)
+ss_res = np.sum(residuals**2)
+ss_tot = np.sum((reciprocal_t_star-np.mean(reciprocal_t_star))**2)
+r_squared = round(1 - (ss_res / ss_tot), 2)
+#%%
 #Plotting 1/t vs. freq
+max_freq=round(float(freq[reciprocal_t_star==max(reciprocal_t_star)]),4)
+min_freq=round(float(freq[freq==1.8920666666666663]),4)
 fig, ax = plt.subplots(figsize=(10,10))
-ax.plot(freq_new, 1/focus_time_new, 'ro', markersize='10')
-ax.plot(xdata, ydata, 'r--')
+ax.plot(freq, reciprocal_t_star,'ro', markersize='10', label='Corrected data')
+ax.plot(freq[reciprocal_t_star==max(reciprocal_t_star)], reciprocal_t_star[reciprocal_t_star==max(reciprocal_t_star)],'o', markersize='10', label=f'Min focussing time (f = {max_freq} MHz)')
+ax.plot(freq[freq==1.8920666666666663], reciprocal_t_star[freq==1.8920666666666663], color= 'tab:green', marker='o', markersize='10', label=f'Max focussing time (f = {min_freq} MHz)')
+ax.plot(xdata, ydata, linestyle='dashdot', color ='black', label=f'Fit ($r^2$={r_squared})')
 
 ax.set_xlabel(r'Frequency [MHz]', fontsize='17.5')
-ax.set_ylabel(r'1/Time [$\mathrm{s}^{-1}$]', fontsize='17.5')
-
-plt.savefig('path', dpi=300, bbox_inches='tight')
+ax.set_ylabel(r'1/$t^*$ [$\mathrm{s}^{-1}$]', fontsize='17.5')
+plt.legend()
+plt.savefig('/Users/joakimpihl/Desktop/DTU/7. Semester/Bachelorprojekt/Results/Focus sweep 6.3/Resonance.png', dpi=300, bbox_inches='tight')
 #Generate csv file with filtered data
-df_write = pd.DataFrame({'Frequency (MHz)':freq_new, 'Focusing time (99.99%)':focus_time_new})
-df_write.to_csv('path', sep=',')
+#df_write = pd.DataFrame({'Frequency (MHz)':freq_new, 'Focusing time (99.99%)':focus_time_new})
+#df_write.to_csv('path', sep=',')
