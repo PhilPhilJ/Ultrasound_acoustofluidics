4680db124d0e01209369a67398c6a5d23edf9a54
JEPihl <141318950+JEPihl@users.noreply.github.com>
2024-03-13 16:01:30 +0100

    Adds cropping of the video to the script

--- automated flow with vid analysis.py
+++ automated flow with vid analysis.py
@@ -18,6 +18,27 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 # Define functions
+
+def update(val): # The function to be called anytime a slider's value changes
+    global slider_val
+    slider_val = val
+    vert_line.set_xdata(val)
+    fig.canvas.draw_idle()
+    
+def assign_min(event): #A function that assigns the threshold value given by the slider
+    global index_left_side
+    index_left_side = int(globals()['slider_val'])
+    print('The min threshold has been set to: ' + str(round(index_left_side,0)))
+    if 'index_right_side' in globals() and 'focus_point' in globals():
+        plt.close(fig)
+
+def assign_max(event): #A function that assigns the threshold value given by the slider
+    global index_right_side
+    index_right_side = int(globals()['slider_val'])
+    print('The max threshold has been set to: ' + str(round(index_right_side,0)))
+    if 'index_left_side' in globals() and 'focus_point' in globals():
+        plt.close(fig)
+
 # Function to find the mean value of the frame
 def mean_value(frame, ratio=0.2):
     center_col = frame.shape[1] // 2
@@ -44,8 +65,61 @@ def func(t, t_star, R, ratio=0.2):
     k = np.pi*(1-ratio)/2
     return 1 - R/k * np.arctan(np.tan(k) * np.exp(-t/t_star))
 
+# conecting to the first available camera
+camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+converter = pylon.ImageFormatConverter()
 
+#################### Crop image data ####################
+camera.StartGrabbingMax(countOfImagesToGrab)
+# converting to opencv bgr format
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
 
+while camera.IsGrabbing():
+    grabResult = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+    if grabResult.GrabSucceeded():
+        # Access the image data
+        image = converter.Convert(grabResult)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = image.GetArray()
+        k = cv2.waitKey(1)
+        if k == 27:
+            break
+    grabResult.Release()
+
+fig, ax1 = plt.subplots()
+fig.set_size_inches(20/2.54, 15/2.54)
+
+ax1.set_xlabel('x-direction [px]') #Image of channel
+ax1.set_ylabel('y-dricetion [px]')
+ax1.imshow(img, cmap='gray')
+
+ax2 = ax1.twinx() #Intensities
+ax2.set_ylabel('Intensity [0;255]')
+ax2.plot(np.arange(0, frame_length), np.mean(frame, axis=0), color='r')
+
+#ax3 = ax1.twinx() #Intensities
+ax3 = ax1.twinx()
+ax3.axis('off')
+vert_line = plt.axvline(x=frame_length/3, color = 'b')
+
+# Make a horizontal slider to control the line position.
+axpos = fig.add_axes([0.2, 0, 0.65, 0.03])
+pos_slider = Slider(ax=axpos, label='Position', valmin=0, valmax=frame_length, valinit=frame_length/3)
+
+# register the update function with each slider
+pos_slider.on_changed(update)
+
+# Create a `matplotlib.widgets.Button` to accept the slider value.
+min_val = fig.add_axes([0.0125, 0.05, 0.1, 0.04])
+max_val = fig.add_axes([0.875, 0.05, 0.1, 0.04])
+button_min = Button(min_val, 'Min', hovercolor='0.975')
+button_max = Button(max_val, 'Max', hovercolor='0.975')
+
+button_min.on_clicked(assign_min)
+button_max.on_clicked(assign_max)
+
+##################### Main script #####################
 print('Press ESC to close the window')
 
 arduinoSer = serial.Serial('COM3',baudrate=9600,timeout=0.5)
@@ -60,23 +134,20 @@ fitting_parameters = np.array([[],[]]) # array to store the fitting parameters
 
 frequency = 1.8975 # frequency in MHz  
 runs = 2
-    
-for i in range(runs):
 
-    # Define array to store the mean values
-    mean_values = np.array([])
+# Define array to store the mean values
+mean_values = np.array([])
         
-    # conecting to the first available camera
-    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
-        
-    # Grabing Continusely (video) with minimal delay
-    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
-    converter = pylon.ImageFormatConverter()
 
-    # converting to opencv bgr format
-    converter.OutputPixelFormat = pylon.PixelType_BGR8packed
-    converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
-        
+# Grabing Continusely (video) with minimal delay
+camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+
+# converting to opencv bgr format
+converter.OutputPixelFormat = pylon.PixelType_BGR8packed
+converter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+for i in range(runs):       
+
     newpath = r'C:/Users/s102772/Desktop/Test'#r"D:/New Script/"
     if not os.path.exists(newpath):
         os.makedirs(newpath)
@@ -131,6 +202,8 @@ for i in range(runs):
             # Access the image data
             image = converter.Convert(grabResult)
             img = image.GetArray()
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            img = img[:, index_left_side:index_right_side]
             cv2.namedWindow('Live view', cv2.WINDOW_NORMAL)
             cv2.imshow('Live view', img)
             k = cv2.waitKey(1)
