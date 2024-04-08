dae5fa6f8b4c388ffc23d60ec1385b8007d8f106
PhilPhilJ <113350944+PhilPhilJ@users.noreply.github.com>
2024-04-08 14:04:48 +0200

    Update Test_volt_diff.ino

--- Test_volt_diff/Test_volt_diff.ino
+++ Test_volt_diff/Test_volt_diff.ino
@@ -1,5 +1,5 @@
-int tc_pin_1 = 5; // Pins thermocouples are allocated
-int tc_pin_2 = 6;
+int tc_pin_1 = 0; // Pins thermocouples are allocated
+int tc_pin_2 = 2;
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,10 +15,14 @@ void loop() {
   // Convert analog voltage (0-1023) to volts (0-5V)
   float tc_volt_1 = tc_sensor_1 * (5.0 / 1023);
   float tc_volt_2 = tc_sensor_2 * (5.0 / 1023);
-  float volt_diff = tc_volt_2 - tc_volt_1;
+  float volt_diff = abs(tc_volt_2 - tc_volt_1)*1000 ;
   // print voltages
-  String ouput = 'Pin 1 = ' + tc_volt_1 + ' Pin 2 = ' + tc_volt_2 + ' Difference = ' + volt_diff;
-  Serial.println(output);
+  Serial.println("Pin 1 voltage (V)");
+  Serial.println(tc_volt_1, 5);
+  Serial.println("Pin 2 voltage (V)");
+  Serial.println(tc_volt_2, 5);
+  Serial.println("Volt diff (mV)");
+  Serial.println(volt_diff, 5);
 
   delay(5000); 
 }
