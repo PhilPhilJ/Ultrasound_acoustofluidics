90887a5927d357b5c7e4310bdfc322a72d269e8a
PhilPhilJ <113350944+PhilPhilJ@users.noreply.github.com>
2024-04-24 15:08:34 +0200

    Update V_read_arduino.py

--- V_read_arduino.py
+++ V_read_arduino.py
@@ -9,9 +9,14 @@ arduino = serial.Serial(port, baudrate, timeout=0.5)
 
 # Main loop
 while True:
+    V_in = 5
     # Read the voltage from the analog pins
-    voltage1 = arduino.readline().decode().strip()
-    voltage2 = arduino.readline().decode().strip()
+    voltage1 = float(arduino.readline().decode().strip())
+    voltage2 = float(arduino.readline().decode().strip())
+    voltage4 = float(arduino.readline().decode().strip())
+    
+    v_termo = abs(voltage1-voltage2)
+    v_diff_abs = V_in-voltage4
     
     # Print the voltage values
     #print(f"Voltage on pin 0: {voltage1}")
